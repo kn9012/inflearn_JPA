42f683fdc6942dd6f96f70d6bac1bb811877cc43
kn9012 <kn9012@naver.com>
2024-02-22 00:03:16 +0900

    컬렉션 조회 최적화

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -66,6 +66,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDto();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @GetMapping("/api/v3.1/orders")
     public List<OrderDto> ordersV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
                                         @RequestParam(value = "limit", defaultValue = "100") int limit
